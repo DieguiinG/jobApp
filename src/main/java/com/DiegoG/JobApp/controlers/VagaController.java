765ba0a24d964524629528c3be514ed21d37cc52
DieguiinG <difejacyntho@hotmail.com>
2023-11-18 18:29:45 -0300

    Cadastro de vagas coletando dados

--- src/main/java/com/DiegoG/JobApp/controlers/VagaController.java
+++ src/main/java/com/DiegoG/JobApp/controlers/VagaController.java
@@ -3,17 +3,28 @@ package com.DiegoG.JobApp.controlers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.DiegoG.JobApp.models.Vaga;
 import com.DiegoG.JobApp.repositories.VagaRepository;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class VagaController {
 
 	@Autowired
-	VagaRepository vagaRepository;
-	
-	@GetMapping("/")
-    public String index() {
-        return "index";
-    }
+	VagaRepository vr;
+
+	@GetMapping("/cadastroVaga")
+	public String cadastroVaga() {
+		return "/cadastroVaga";
+	}
+
+	@PostMapping("/cadastroVaga")
+	public String formVaga(@Valid Vaga vaga) {
+		vr.save(vaga);
+		return "redirect:/cadastroVaga";
+		
+	}
 }
